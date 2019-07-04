1b0cf535d588d0973efeba70ff6df8bcc3bc4b2b
Ludovic Courtès <ludo@gnu.org>
2019-07-04 14:37:09 +0200

    build: Return a <computed-file>.
    * guix.scm: Wrap gexp in 'computed-file'.

--- guix.scm
+++ guix.scm
@@ -1,7 +1,7 @@
 ;;; This module is part of Guix-HPC and is licensed under the same terms,
 ;;; those of the GNU GPL version 3 or (at your option) any later version.
 ;;;
-;;; Copyright © 2017 Inria
+;;; Copyright © 2017, 2019 Inria
 
 ;; Run 'guix build -f guix.scm' to build the web site.
 
@@ -23,30 +23,33 @@
               #:recursive? #t
               #:select? (git-predicate ".")))
 
-(with-imported-modules (source-module-closure
-                        '((guix build utils)))
-  #~(begin
-      (use-modules (guix build utils))
+(define build
+  (with-imported-modules (source-module-closure
+                          '((guix build utils)))
+    #~(begin
+        (use-modules (guix build utils))
 
-      (copy-recursively #$source ".")
+        (copy-recursively #$source ".")
 
-      ;; For Haunt.
-      (setenv "GUILE_LOAD_PATH"
-              (string-append
-               #+(file-append guile-commonmark
-                              "/share/guile/site/2.2")
-               ":"
-               #+(file-append guile-syntax-highlight
-                              "/share/guile/site/2.2")))
+        ;; For Haunt.
+        (setenv "GUILE_LOAD_PATH"
+                (string-append
+                 #+(file-append guile-commonmark
+                                "/share/guile/site/2.2")
+                 ":"
+                 #+(file-append guile-syntax-highlight
+                                "/share/guile/site/2.2")))
 
-      ;; So we can read/write UTF-8 files.
-      (setenv "GUIX_LOCPATH"
-              #+(file-append (specification->package "glibc-utf8-locales")
-                             "/lib/locale"))
-      (setenv "LC_ALL" "en_US.utf8")
+        ;; So we can read/write UTF-8 files.
+        (setenv "GUIX_LOCPATH"
+                #+(file-append (specification->package "glibc-utf8-locales")
+                               "/lib/locale"))
+        (setenv "LC_ALL" "en_US.utf8")
 
-      (and (zero? (system* #+(file-append haunt "/bin/haunt")
-                           "build"))
-           (begin
-             (mkdir-p #$output)
-             (copy-recursively "site" #$output)))))
+        (and (zero? (system* #+(file-append haunt "/bin/haunt")
+                             "build"))
+             (begin
+               (mkdir-p #$output)
+               (copy-recursively "site" #$output))))))
+
+(computed-file "guix-hpc-web-site" build)
